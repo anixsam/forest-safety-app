2ab2a591702775cc0b91b3de79b78f33c2ae7603
Anix <Anix@self.com>
2023-02-26 18:08:25 +0530

    Initial Boilerplate template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MainComponent } from './components/main/main.component';
+import { WebcamComponent } from './components/webcam/webcam.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { WebcamModule } from 'ngx-webcam';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    WebcamComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    WebcamModule
   ],
   providers: [],
   bootstrap: [AppComponent]
